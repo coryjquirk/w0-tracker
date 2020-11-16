5b907bb2e7b302dea57b896757b82cbdf3fecf64
Cory Quirk <coryjquirk@gmail.com>
2020-11-16 00:15:07 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+//node packages
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+//express
+const app = express();
+const PORT = process.env.PORT || 8000;
+//express data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(logger("dev"));
+
+//routes
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
+
+//mongoose bp
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+//server listening?
+app.listen(PORT, () => {
+    console.log(`App running at http://localhost:${PORT}!`);
+});
